fea84f757326ac8af2d770ddf7f4160712679d3c
JPBago <bagolon@gmail.com>
2023-10-02 21:12:59 -0300

    Conexion a la Base de Datos

--- src/grupo76_cuartelbomberos/Grupo76_CuartelBomberos.java
+++ src/grupo76_cuartelbomberos/Grupo76_CuartelBomberos.java
@@ -1,9 +1,18 @@
 
 package grupo76_cuartelbomberos;
+
+import grupo76_cuartelbomberos.coneccion.coneccionData;
+import java.sql.Connection;
+
 public class Grupo76_CuartelBomberos {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Connection con=coneccionData.Conectar();
+        
+        coneccionData.cerrarConexion();
+    
     }
 
 }
--- src/grupo76_cuartelbomberos/coneccion/coneccionData.java
+++ src/grupo76_cuartelbomberos/coneccion/coneccionData.java
@@ -1,6 +1,48 @@
-
 package grupo76_cuartelbomberos.coneccion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class coneccionData {
 
+    private static final String URL = "jdbc:maiadb://localhost/";
+    private static final String DB = "bomberos";//ver si la base de datos coinside
+    private static final String USER = "root";
+    private static final String PASS = "";
+    private static Connection connection;
+
+    public coneccionData() {
+    }
+
+    public static Connection Conectar() {
+        if (connection == null) {
+            try {
+                // cargar Driver de Conexion
+                Class.forName("org.mariadb.jdbc.Driver");
+                System.out.println("Drivers cargados exitosamente.");
+                // Conectar al servidor de la base de datos
+                connection = DriverManager.getConnection(URL + DB, USER, PASS);
+                System.out.println("Conexión establecida con exito.");
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Fallo al cargar los drivers de maridb !!\n" + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Fallo al conectar con la Base de Datos !!\n" + ex.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    public static void cerrarConexion() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Se desconectó de la Base de Datos !!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al intentar cerrar la base de datos.\n" + ex.getMessage());
+        }
+    }
 }
