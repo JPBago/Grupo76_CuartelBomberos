9d70fc4303787f519e4e599c7fe650433e58788c
JPBago <bagolon@gmail.com>
2023-10-03 19:46:51 -0300

    c

--- src/grupo76_cuartelbomberos/entidades/Bomberos.java
+++ src/grupo76_cuartelbomberos/entidades/Bomberos.java
@@ -37,6 +37,70 @@ public class Bomberos {
         this.activo = activo;
     }
 
+    public int getCodBombero() {
+        return codBombero;
+    }
+
+    public void setCodBombero(int codBombero) {
+        this.codBombero = codBombero;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombreApe() {
+        return nombreApe;
+    }
+
+    public void setNombreApe(String nombreApe) {
+        this.nombreApe = nombreApe;
+    }
+
+    public String getGrupoSang() {
+        return grupoSang;
+    }
+
+    public void setGrupoSang(String grupoSang) {
+        this.grupoSang = grupoSang;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public Brigada getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(Brigada codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
 
     @Override
     public String toString() {
